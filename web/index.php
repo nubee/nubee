b9cb7d1e6d2961c3620e1624036eda12d9e2ecce
root <root@vm203133.planetacomnetwork.com>
2010-03-09 08:53:17 +0100

    fixed permissions

--- web/index.php
+++ web/index.php
@@ -2,5 +2,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();
